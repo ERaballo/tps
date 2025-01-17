638555e5cd210737406444c1647a6a49d50d9abf
Enrico Raballo <enrico.raballo07@gmail.com>
2025-01-17 11:20:00 +0300

    Update script.js

--- stella 4Einf/script.js
+++ stella 4Einf/script.js
@@ -2,12 +2,11 @@ const canvas = document.getElementById("Canvas");
 const ctx = canvas.getContext("2d");
 
 ctx.moveTo(240, 75);
-ctx.lineTo(350, 425);
-ctx.lineTo(75,200);
-ctx.lineTo(405,200);
-ctx.lineTo(117, 425);
+ctx.lineTo(345, 390);
+ctx.lineTo(71,205);
+ctx.lineTo(409,205);
+ctx.lineTo(133, 390);
 ctx.lineTo(240, 75);
-
-
-
+ctx.moveTo(415,250);
+ctx.arc(240, 250, 175, 0, 2 * Math.PI);
 ctx.stroke();
