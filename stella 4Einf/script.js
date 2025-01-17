abac6a6f3d37544283890b9653fd18f377429839
Enrico Raballo <enrico.raballo07@gmail.com>
2025-01-17 10:57:35 +0300

    Add files via upload

---
+++ stella 4Einf/script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.moveTo(240, 75);
+ctx.lineTo(350, 425);
+ctx.lineTo(75,200);
+ctx.lineTo(405,200);
+ctx.lineTo(117, 425);
+ctx.lineTo(240, 75);
+
+
+
+ctx.stroke();
